a3167d267880b2bac778a5988fb4e5d02a857859
Yasmin <yasminmartinsdebrito@gmail.com>
2023-01-03 20:01:15 -0300

    feat:criando um projeto com estilização no console

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log('olá mundo');
+
+import chalk from "chalk";
+
+console.log(chalk.red('olá mundo'));
+console.log(chalk.bgBlue('Yasmin Martins de Brito'));
+console.log(chalk.green('Hellena Victoria'));
