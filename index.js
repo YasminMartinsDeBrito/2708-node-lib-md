d3f27e5259b9382ef47dde04aff937023763f242
Yasmin <yasminmartinsdebrito@gmail.com>
2023-01-04 13:24:32 -0300

    fazendo leitura de arquivos com funções assincronas e sincronas

--- index.js
+++ index.js
@@ -1,6 +1,43 @@
-
+import fs from 'fs';
 import chalk from "chalk";
 
 console.log(chalk.red('olá mundo'));
 console.log(chalk.bgBlue('Yasmin Martins de Brito'));
 console.log(chalk.green('Hellena Victoria'));
+
+function trataErro(erro){
+    console.log(erro)
+    throw new Error(chalk.red(erro.code, 'Não há arquivo no diretório'))
+}
+//async/await
+async function pegaArquivo(caminhoDoArquivo){
+    try{
+        const encoding = 'utf-8'
+        const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
+        console.log(chalk.green(texto))
+    }catch(erro){
+        trataErro(erro)
+    }
+}
+
+
+//promisse con then()
+//    function pegaArquivo(caminhoDoArquivo){
+//        const encoding = 'utf-8';
+//        fs.promises.readFile(caminhoDoArquivo, encoding)
+//        .then((texto) => console.log(chalk.green(texto)))
+//        .catch(trataErro)
+//    }
+
+
+//function pegaArquivo(caminhoDoArquivo){
+//    const encoding = 'utf-8';
+//    fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
+//        if(erro){
+//            trataErro(erro);
+//        }
+//        console.log(chalk.bgYellow(texto));
+//   })
+//
+pegaArquivo('./arquivos/')
+pegaArquivo('./arquivos/texto.md')
---
+++ novo.js
@@ -0,0 +1,15 @@
+function promessa(bool){
+    const x = bool
+    return new Promise((resolve, reject) => {
+        if(!x){
+            reject(new Error("falha na promessa"))
+        }
+        resolve("Sucesso na promessa")
+    })
+}
+
+function exibeResposta(textoResult){
+    console.log(textoResult)
+}
+promessa(true)
+.then((texto) => exibeResposta(texto))
